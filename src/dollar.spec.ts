9723508bc6aba3bac54609d737e27d01451a15f9
paulodutra <paulodutra.info@gmail.com>
2023-05-22 17:02:19 -0300

    test: dollar equality

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -8,5 +8,10 @@ describe('Dollar', () => {
         result = five.times(3)
         expect(result.amount).toEqual(15)
 
-    });
+    })
+
+    it('should return true when dollars of the same value are compared', () => {
+        expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+        expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+    })
 });
