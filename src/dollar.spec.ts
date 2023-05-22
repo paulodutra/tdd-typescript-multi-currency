3897efd2d1314f22271166e6c0a4ebae630768e6
paulodutra <paulodutra.info@gmail.com>
2023-05-22 16:47:59 -0300

    test: remove dollar side-effects

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -2,8 +2,11 @@ import { Dollar } from './dollar'
 
 describe('Dollar', () => {
     it('should return 10 when five dollars are multiplied by two', () => {
-        const five = new Dollar(5)
-        five.times(2)
-        expect(five.amount).toEqual(10)
+        const five: Dollar = new Dollar(5)
+        let result: Dollar = five.times(2)
+        expect(result.amount).toEqual(10)
+        result = five.times(3)
+        expect(result.amount).toEqual(15)
+
     });
 });
