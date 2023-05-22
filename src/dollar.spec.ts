b9799595a2edfad19a3982dcea7a8e905e9d1ed9
paulodutra <paulodutra.info@gmail.com>
2023-05-22 17:27:24 -0300

    refactor: improve test description

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,7 +1,7 @@
 import { Dollar } from './dollar'
 
 describe('Dollar', () => {
-    it('should return 10 when five dollars are multiplied by two', () => {
+    it('should return 10 when five dollars are multiplied by two and 15 when multiple by three', () => {
         const five: Dollar = new Dollar(5)
         expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
         expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
