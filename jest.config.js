ba8b163d25a5061fe87a788aa5f86e598c94319e
paulodutra <paulodutra.info@gmail.com>
2023-05-22 16:36:12 -0300

    chore: configuring jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+module.exports = {
+    roots: ['<rootDir>/'],
+    coverageDirectory: 'coverage',
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: [
+      '<rootDir>/src/*.ts',
+    ],
+    testEnvironment: 'node',
+    transform: {
+      '.+\\.ts$': 'ts-jest'
+    }, 
+}
