1c56060bf7d8c8f104ab86c0f6bac347fdf142f9
paulodutra <paulodutra.info@gmail.com>
2023-05-24 10:03:10 -0300

    test: using euro class

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,10 +1,18 @@
 import { Dollar } from './dollar'
+import { Euro } from './euro'
 
 describe('Dollar', () => {
     it('should return 10 when five dollars are multiplied by two and 15 when multiple by three', () => {
         const five: Dollar = new Dollar(5)
         expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
         expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
+
+    })
+
+    it('should return 10 when five euros are multiplied by two and 15 when multiple by three', () => {
+        const five: Euro = new Euro(5)
+        expect(five.times(2).equals(new Euro(10))).toBeTruthy()
+        expect(five.times(3).equals(new Euro(15))).toBeTruthy()
 
     })
 
