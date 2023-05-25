457a0ddc4e9c95e11cdb09ca0f03122ecd1a91bd
paulodutra <paulodutra.info@gmail.com>
2023-05-25 10:03:16 -0300

    test: handle simple addition

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,21 +1,31 @@
+import { Bank } from './bank'
+import { Expression } from './expression'
 import { Money } from './money'
 
 describe('Money', () => {
-    it('should correctly handle dollar multiplication', () => {
+    it('should handle dollar multiplication', () => {
         const five: Money = Money.dollar(5)
         expect(five.times(2).equals(Money.dollar(10))).toBeTruthy()
         expect(five.times(3).equals(Money.dollar(15))).toBeTruthy()
 
     })
 
-    it('should correctly handle money equality', () => {
+    it('should handle money equality', () => {
         expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
         expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
         expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy()
     })
 
-    it('should correctly handle currencies', () => {
+    it('should handle currencies', () => {
         expect(Money.dollar(1).currency()).toEqual('USD')
         expect(Money.euro(1).currency()).toEqual('EUR')
+    })
+
+    it('should handle simple addition', () => {
+        const five = Money.dollar(5)
+        const sum: Expression = five.plus(five)
+        const bank = new Bank()
+        const reduced: Money = bank.reduce(sum, 'USD')
+        expect(reduced.equals(Money.dollar(10))).toBeTruthy()
     })
 });
